d9ed369af1672e33886fcfdcefd0dff249bee82a
hashy0917 <oohasi3939@gmail.com>
2021-12-15 20:46:41 +0900

    fix dynamic inventory script

--- build-k8s/inventry.sh
+++ build-k8s/inventry.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 if [ -e $PWD/terraform.tfstate ]; then
-    ip=`cat $PWD/terraform.tfstate | jq '.resources[].instances[].attributes["default_ip_address"]' 2> /dev/null | sed -e 's/"//g'| sed -ze 's/null\n//g' | head -n 1`
+    master_ip=`cat $PWD/terraform.tfstate | jq '.resources[].instances[].attributes["default_ip_address"]' 2> /dev/null | sed -e 's/"//g'| sed -ze 's/null\n//g' | head -n 1`
+    node_ip=`cat $PWD/terraform.tfstate | jq '.resources[].instances[].attributes["default_ip_address"]' 2> /dev/null | sed -e 's/"//g'| sed -ze 's/null\n//g' | tail -n 2 | sed -ze 's/\n/,/g'`
     cat <<EOS
 {
-    "master"  : [ $ip ]
-}
-EOS
-fi
-if [ -e $PWD/terraform.tfstate ]; then
-    ip=`cat $PWD/terraform.tfstate | jq '.resources[].instances[].attributes["default_ip_address"]' 2> /dev/null | sed -e 's/"//g'| sed -ze 's/null\n//g' | tail -n 2 | sed -ze 's/\n/,/g'`
-    cat <<EOS
-{
-    "worker"  : [ $ip ]
+    "master"  : [ $master_ip ],
+    "worker"  : [ $node_ip ]
 }
 EOS
 fi
